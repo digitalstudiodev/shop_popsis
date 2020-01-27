8780a815bee630925fdca19baabca5f40e407ddb
jose-dom <josedom000@gmail.com>
2020-01-26 19:43:11 -0500

    Initial Commit

---
+++ core/urls.py
@@ -0,0 +1,47 @@
+from django.urls import path, include
+from .views import (
+    HomeView, 
+    ShopView,
+    SaleView,
+    CheckoutView, 
+    ItemDetailView, 
+    add_to_cart,
+    remove_from_cart,
+    remove_single_item_from_cart,
+    OrderSummaryView,
+    PaymentView,
+    AddCouponView,
+    RequestRefundView,
+    shipping_return,
+    user_profile,
+    OrderDetailView,
+    privacy_policy,
+    terms_use,
+    about_us,
+    contact
+    )
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('shop/', ShopView.as_view(), name='shop'),
+    path('last-chance/', SaleView.as_view(), name='last-chance'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('add-coupon/', AddCouponView.as_view(), name='add-coupon'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('remove-single-item-from-cart/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-cart'),
+    path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
+    path('request-refund/', RequestRefundView.as_view(), name='request-refund'),
+    path('shipping-return/', shipping_return, name='shipping-return'),
+    path('user-profile/', user_profile, name='user-profile'),
+    path('order/<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
+    path('accounts/', include('allauth.urls')),
+    path('privacy-policy/', privacy_policy, name='privacy-policy'),
+    path('terms-use/', terms_use, name='terms-use'),
+    path('about-us/', about_us, name='about-us'),
+    path('contact/', contact, name='contact'),
+]

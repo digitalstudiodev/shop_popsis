464ab8e2744be998e462fd3726f6d57823d68df0
jose-dom <josedom000@gmail.com>
2020-02-08 15:30:53 -0500

    Create Inovice

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -165,6 +165,6 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3h.S3Boto3Storage'
 
 django_heroku.settings(locals())

a71d7c5b0bc8708db45696ce35458b8a7bf0b949
jose-dom <josedom000@gmail.com>
2020-06-29 17:49:00 -0400

    update prices

--- core/models.py
+++ core/models.py
@@ -58,18 +58,18 @@ class OrderItem(models.Model):
         return f"{self.quantity} of {self.item.title}"
 
     def get_total_item_price(self):
-        return self.quantity * self.item.price
+        return "{:.2f}".format(self.quantity * self.item.price)
     
     def get_total_discount_item_price(self):
-        return self.quantity * self.item.discount_price
+        return "{:.2f}".format(self.quantity * self.item.discount_price)
 
     def get_amount_saved(self):
         return "{:.2f}".format(self.get_total_item_price() - self.get_total_discount_item_price())
     
     def get_final_price(self):
         if self.item.discount_price:
-            return self.get_total_discount_item_price()
-        return self.get_total_item_price()
+            return "{:.2f}".format(self.get_total_discount_item_price())
+        return "{:.2f}".format(self.get_total_item_price())
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -99,12 +99,12 @@ class Order(models.Model):
             total += round(order_item.get_final_price(), 2)
         if self.coupon:
             total -= round(self.coupon.amount, 2)
-        return total
+        return "{:.2f}".format(total)
     
     def tax_total(self):
         tax = 0 
         tax += round((self.get_total() * 0.07), 2)
-        return tax
+        return "{:.2f}".format(tax)
 
     def shipping(self):
         return 5
@@ -112,7 +112,7 @@ class Order(models.Model):
     def get_absolute_total(self):
         absolute_total = 0
         absolute_total += round((self.get_total() + self.tax_total() + self.shipping()), 2)
-        return absolute_total
+        return "{:.2f}".format(absolute_total)
 
 
 class Address(models.Model):

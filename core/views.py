dc47f89797f8ab29d5f03eacf9fb740e6daae63b
Jose Dominguez <josedominguez@esddvn198l01.rad.rutgers.edu>
2020-11-03 01:00:43 -0500

    sources is not valid

--- core/views.py
+++ core/views.py
@@ -239,11 +239,11 @@ class PaymentView(View):
             if save:
                 if userprofile.stripe_customer_id != '' and userprofile.stripe_customer_id is not None:
                     customer = stripe.Customer.retrieve(userprofile.stripe_customer_id)
-                    customer.sources.create(source=token)
+                    customer.create(source=token)
 
                 else:
                     customer = stripe.Customer.create(email=self.request.user.email,)
-                    customer.sources.create(source=token)
+                    customer.create(source=token)
                     userprofile.stripe_customer_id = customer['id']
                     userprofile.one_click_purchasing = True
                     userprofile.save()

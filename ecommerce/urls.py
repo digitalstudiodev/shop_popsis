faf4883fa108d940c52ad8b0dd29f244c564a8c5
Jose Dominguez <josedominguez@MacBook-Pro.home>
2020-11-08 19:46:25 -0500

    update urls

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -22,7 +23,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
     path('users/', include('users.urls', namespace='users')),
-    path('dashboard/', include('dashboard.urls', namespace='dashboard'))
+    path('dashboard/', include('dashboard.urls', namespace='dashboard')),
+    #reset password
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="users/password_reset/request_reset.html"), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset/request_reset_done.html"), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset/request_reset_confirm.html"), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset/request_reset_complete.html"), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:

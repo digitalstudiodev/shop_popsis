d0c0e2f1da1e7ca7ab3d412fe46b51aa1160a698
Jose Dominguez <josedominguez@MacBook-Pro.home>
2020-11-09 10:56:39 -0500

    update debug

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_PUBLISHABLE_KEY = os.environ.get('STRIPE_PUBLISHABLE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE')
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['free-soul.herokuapp.com','*','localhost/','https://www.shoppopsis.com/']
 

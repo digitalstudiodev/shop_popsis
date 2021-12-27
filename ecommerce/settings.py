5a4623685f07b6f6eced0560474cde180a01d22a
jose-dom <jrd251@rutgers.edu>
2021-12-26 23:55:09 -0500

    update storage

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -155,7 +155,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'#'whitenoise.storage.CompressedManifestStaticFilesStorage'#'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'#'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

a824c9fcdf4bc636dea35434aa89fdb7e9e57da3
jose-dom <jrd251@rutgers.edu>
2021-12-29 12:26:44 -0500

    update settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY =os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False#os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['https://www.shoppopsis.com/','https://popsis.herokuapp.com']
+ALLOWED_HOSTS = ['https://www.shoppopsis.com/','https://popsis.herokuapp.com',"*"]
 
 # Application definition
 
@@ -122,7 +122,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-#Static and Media Config Settings
+# Static and Media Config Settings
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -156,7 +156,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
-DEFAULT_FILE_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'#'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' #'whitenoise.storage.CompressedManifestStaticFilesStorage'#
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

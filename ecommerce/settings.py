e09ef770bb12b7527a29625a97c24ff79d25beed
jose-dom <jrd251@rutgers.edu>
2022-02-07 12:37:53 -0500

    update debug

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY =os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True#os.environ.get('DEBUG')
+DEBUG = False #os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['https://popsis.herokuapp.com','https://www.shoppopsis.com/','www.shoppopsis.com','shoppopsis.com','popsis.herokuapp.com']
 
